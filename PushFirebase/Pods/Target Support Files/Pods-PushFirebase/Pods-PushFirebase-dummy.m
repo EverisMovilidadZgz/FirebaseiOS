aaa605cc63a1b86e1082f7a91d2ac9522b809a57
Javier Goñi Puerta <javier.goni.puerta@everis.com>
2018-05-14 11:41:52 +0200

    Start project

---
+++ PushFirebase/Pods/Target Support Files/Pods-PushFirebase/Pods-PushFirebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PushFirebase : NSObject
+@end
+@implementation PodsDummy_Pods_PushFirebase
+@end
---
+++ PushFirebase/PushFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushFirebase
+//
+//  Created by Javier Goñi Puerta on 14/5/18.
+//  Copyright © 2018 Javier Goñi Puerta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
